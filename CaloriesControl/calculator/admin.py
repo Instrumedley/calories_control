ff9b40c7f8ee07cd2313be097b914ee48376fbf7
Rafael Santos <rafaelalfredo2008@gmail.com>
2016-08-02 21:13:51 +0200

    -Created superuser for admin -Migrated model Food -added 3 items on Food via admin

--- CaloriesControl/calculator/admin.py
+++ CaloriesControl/calculator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Food
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name','quantity','unity','calories_per_unity')
+
+admin.site.register(Food, FoodAdmin)
--- CaloriesControl/calculator/models.py
+++ CaloriesControl/calculator/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    unity = models.CharField(max_length=20)
+    calories_per_unity = models.IntegerField()
+
+
