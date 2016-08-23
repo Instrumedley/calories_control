5cf5a33210392ab57bd1f49a83fcd9210e5f0640
Rafael Santos <rafaelalfredo2008@gmail.com>
2016-08-23 14:02:44 +0200

    -Did a bunch of refactoring, improved WebScrapper.py and changed some settings

--- CaloriesControl/CaloriesControl/wsgi.py
+++ CaloriesControl/CaloriesControl/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+#sys.path.append('~/Documents/workspace/CaloriesControl/CaloriesControl')
+#sys.path.append('~/Documents/workspace/CaloriesControl/CaloriesControl/CaloriesControl')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CaloriesControl.settings")
 
 application = get_wsgi_application()
+
--- CaloriesControl/calculator/WebScrapper.py
+++ CaloriesControl/calculator/WebScrapper.py
@@ -1,8 +1,16 @@
 __author__ = 'Rafael'
 
+
 from lxml import html
 import requests
 import re
+import os
+
+
+
+
+
+from .models import Food
 
 
 def clean_data_from_web_source(datalist_names, datalist_calories, datalist_quantity):
@@ -24,10 +32,82 @@ def clean_data_from_web_source(datalist_names, datalist_calories, datalist_quant
         quantity_list.append(re.findall('\d+', quantity))
         unity_list.append(re.findall("[a-zA-Z]+", quantity))
 
+    for idx,value in enumerate(quantity_list):
+        if not value:
+            quantity = unity_list[idx]
+
+            # case 1: cup measurements
+            if "cup" in quantity:
+                if "Half" in quantity:
+                    quantity_list[idx] = 0.5
+                elif "1" or "One" in quantity:
+                    quantity_list[idx] = 1
+                elif "Quarter" in quantity:
+                    quantity_list[idx] = 0.25
+
+                unity_list[idx] = "cup"
+
+            # case 2: 'One big'
+            elif "big" in quantity and "One" in quantity:
+                quantity_list[idx] = 1
+                datalist_names[idx] = datalist_names[idx].join(" Big")
+                unity_list[idx] = ""
+
+            # case 3: 'Medium g'
+            elif "g" in quantity and "Medium" in quantity:
+                datalist_names[idx] = datalist_names[idx].join(" Medium")
+                unity_list[idx] = ""
+
+            # case 4: 'half a breast'
+            elif "breast" in quantity and "Half" in quantity:
+                quantity_list[idx] = 0.5
+                unity_list[idx] = "breast"
+
+            # case 5: 'A slice'
+            elif "slice" in quantity:
+                quantity_list[idx] = 1
+                unity_list[idx] = "slice"
+
+            # case 6: 'Quarter of a loaf'
+            elif "loaf" in quantity:
+                quantity_list[idx] = 0.25
+                unity_list[idx] = "loaf"
+
+            # case 7: 'Package medium'
+            elif "Package" in quantity:
+                quantity_list[idx] = 1
+                unity_list[idx] = "package"
+
+            # case 8: 'Teaspoon'
+            elif "Teaspoon" in quantity:
+                quantity_list[idx] = 1
+                unity_list[idx] = "teaspoon"
+            # case 9: 'Spoon to eat'
+            elif "Spoon" in quantity:
+                quantity_list[idx] = 1
+                unity_list[idx] = "spoon"
+            # case 10: 'Half'
+            elif "Half" in quantity:
+                quantity_list[idx] = 0.5
+                unity_list[idx] = ""
+
+            # case 11: 'One medium'
+            elif "medium" in quantity and "One" in quantity:
+                quantity_list[idx] = 1
+                unity_list[idx] = "medium"
+
+            else:
+                print idx
+                print unity_list[idx]
+    print datalist_names
     print quantity_list
     print unity_list
 
+
+
     # third thing: replace 'Half a cup' for 0.5 + cup
+
+
 
 # Source #1 : http://www.myfoodbuddy.com/foodCalorieTable.htm
 page = requests.get('http://www.myfoodbuddy.com/foodCalorieTable.htm')
@@ -71,9 +151,10 @@ for div_node in div_node_list_quantity:
     span_node = div_node.getchildren()
     quantity_source2_list.append(span_node[0].text_content())
 
-print food_source2_list
-print calories_source2_list
-print quantity_source2_list
+#print food_source2_list
+#print calories_source2_list
+#print quantity_source2_list
 
 clean_data_from_web_source(food_source2_list,calories_source2_list,quantity_source2_list)
+
 
